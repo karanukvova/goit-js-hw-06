c0eeb67561f30a197493b6bc006e0347f63886fb
karanukvova <volodimirkaranuk@gmail.com>
2023-04-17 18:31:24 +0300

    update

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("font-size-control")
 const span = document.getElementById("text")
+span.style.fontSize = `${input.value}px`
 input.addEventListener("input", onInputChange)
 function onInputChange() {
     span.style.fontSize = `${input.value}px`
